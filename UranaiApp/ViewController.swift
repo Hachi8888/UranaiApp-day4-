0e8b206d8658b1af028571fb1f0ea053f3352588
Hachi8888 <h.nana.420@gmail.com>
2019-08-09 16:14:41 +0900

    各選択肢の占い結果を配列で定義

--- UranaiApp/ViewController.swift
+++ UranaiApp/ViewController.swift
@@ -18,9 +18,10 @@ class ViewController: UIViewController {
     // 血液型を選択するSegmentControllを紐付け
     @IBOutlet weak var bloodType: UISegmentedControl!
     
+    
     // Sliderから好きな数字を選ぶとSliderの下のラベルに数字が反映される
     @IBAction func likeNum(_ sender: UISlider) {
-        // 下のラベル(showNum)に文字を反映させる処理を書く(Int→Srtingにキャストもする)
+        // 下のラベル(showNum)に文字を反映させる処理を書く(Int型→Srting型にキャストもする)
         showNum.text = String(sender.value)
         
     }
@@ -35,6 +36,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var result: UITextView!
     
     
+    //   それぞれにデータを並列させる
+    let pickerResult : [String] = [
+        "あなたは山に愛されています。\n",
+        "あなたは海に愛されています。\n",
+        "あなたは川に愛されています。\n",
+        "あなたは森に愛されています。\n"
+    ]
+    
+    
+    let bloodResult : [String] = [
+        "属性は「雷」です。\n",
+        "属性は「水」です。\n",
+        "属性は「風」です。\n",
+        "属性は「草」です。\n"
+    ]
+    
+    let likeNumResult: [String] = [
+        "努力が報われなかったときは、「自分へのごほうび」とつぶやきながら、ひと口サイズのスイーツを口にして。",
+        "探し物を思い浮かべながら、人差し指を7回反時計回りにまわしてみて。",
+        "銀製品や銀のアクセサリーをピカピカに磨いてから、バッグに入れたり身につけて。",
+        "ふと口をついて出た曲をダウンロードするか、ラジオ番組にリクエストメールを出しましょう。",
+    ]
     
     
     
