82391dd7eb4efc4f2bc55667c23d70b16e50b24c
Hachi8888 <h.nana.420@gmail.com>
2019-08-09 12:57:43 +0900

    first commit

---
+++ UranaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UranaiApp
+//
+//  Created by VERTEX22 on 2019/08/09.
+//  Copyright © 2019 N-project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
