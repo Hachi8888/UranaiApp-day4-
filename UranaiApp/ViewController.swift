11ef2b4728ee7d1865b982e996e2e28a9ca0ccf2
Hachi8888 <h.nana.420@gmail.com>
2019-08-10 09:01:24 +0900

    // 画面が読み込まれたときの処理

--- UranaiApp/ViewController.swift
+++ UranaiApp/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     // ★紐付け一覧
     // 誕生日を入力するPickerViewを紐付け
-    @IBOutlet weak var birthDayPickerView: UIDatePicker!
+    @IBOutlet weak var birthDayPicker: UIDatePicker!
+    var sliderNum: Int = 50
+    
     
     // 血液型を選択するSegmentControllを紐付け
     @IBOutlet weak var bloodType: UISegmentedControl!
@@ -22,9 +24,9 @@ class ViewController: UIViewController {
     @IBAction func likeNum(_ sender: UISlider) {
         // 下のラベル(showNum)に文字を反映させる処理を書く
          // sender.valueの初期値はDouble型なのでInt型へキャストする
-         let likeNum = Int(sender.value)
+         sliderNum = Int(sender.value)
          // textに代入するためにSrting型にキャストする
-         showNum.text = String(likeNum)
+         showNum.text = String(sliderNum)
         
     }
     
@@ -35,9 +37,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var result: UITextView!
     
     
+    // ★変数、定数一覧
     
     
-    //  ★各選択肢の占い結果を配列で定義
+    //  各選択肢の占い結果を配列で定義
     // 誕生日の選択結果
     let pickerResult : [String] = [
         "あなたは山に愛されています。\n",
@@ -66,40 +69,61 @@ class ViewController: UIViewController {
     
     // ★関数
     
-    // birthDayPickerViewの選択結果を、占い結果を決めるkeyを戻り値として取得する(returnするので、戻り値の型指定も忘れないこと!!)
-    fileprivate func setPicker() -> Int{
-        // まず、birthDayPickerViewの全てのデータを格納する(PickerViewの紐付け時に型をUIDatePicker!にすること)
-        let date = birthDayPickerView.date
-        
-        // calandar.componentで上記で定義した定数dateから必要なデータ(年・月・日)を取り出す
-        let year = birthDayPickerView.calendar.component(.year, from: date)
-        let month = birthDayPickerView.calendar.component(.month, from: date)
-        let day = birthDayPickerView.calendar.component(.day, from: date)
-        
-        // 占い結果(に使うため、4で割った余りを定数に格納(4パターンある)
-        let pickerKey: Int = (year + month + day) % 4
-        
-        return pickerKey
-    }
-    
-    // bloodTypeの選択の選択結果を、占い結果を決めるkeyを戻り値として取得する
-     fileprivate func setBlood() -> Int {
-        
-        // 占い結果(に使うため、bloodTypeのセグメントで選択されたインデックスを取得し、定数に格納(4パターンある)
-        let bloodKey: Int = bloodType.selectedSegmentIndex
-        return bloodKey
-    }
-    
-    
-    
     // 画面が読み込まれたときの処理
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // Slider(lineNum)の初期値を設定
-        showNum.text = "50"
     }
+    
+    // birthDayPickerViewの選択結果を、占い結果を決めるkeyに変換し、戻り値で取得する(returnするので、戻り値の型指定も忘れないこと!!)
+    
+    fileprivate func setPicker() -> Int{
+        // まず、birthDayPickerViewの全てのデータを格納する(PickerViewの紐付け時に型をUIDatePicker!にすること)
+        let date = birthDayPicker.date
+        
+        // calandar.componentで上記で定義した定数dateから必要なデータ(年・月・日)を取り出す
+        let year = birthDayPicker.calendar.component(.year, from: date)
+        let month = birthDayPicker.calendar.component(.month, from: date)
+        let day = birthDayPicker.calendar.component(.day, from: date)
+        
+        
+        // 占い結果(に使うため、4で割った余りを定数に格納(4パターンある)
+        let key: Int = (year + month + day) % 4
+        
+        return key
+    }
+    
+    // bloodTypeの選択の選択結果を、占い結果を決めるkeyに変換し、戻り値で取得する
+     fileprivate func setBlood() -> Int {
+        
+        // 占い結果(に使うため、bloodTypeのセグメントで選択されたインデックスを取得し、定数に格納(4パターンある)
+        let Key: Int = bloodType.selectedSegmentIndex
+        return Key
+    }
+    
+    
+     // Sliderから選んだ数字の選択結果を、占い結果を決めるkeyに変換し、戻り値で取得する
+    fileprivate func setLikeNum() -> Int {
+
+        let Key: Int = sliderNum % 4
+
+
+        return Key
+    }
+    
+    
+     //「占うボタン」をおしたとき、占い結果を最下部のtextView(result)に表示する
+    @IBAction func finalResult(_ sender: Any) {
+
+        result.text = pickerResult[setPicker()] + bloodResult[setBlood()] + likeNumResult[setLikeNum()]
+
+    }
+    
+    
+    
+    
+
 
 
 }
